f0cd8d9b0d559ae81a9f3fa6be374d222956b16c
Mohammad Kamran Shekh <kshekh@gmail.com>
2024-12-12 15:54:34 +0530

    UI updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,33 +8,13 @@ const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
-
+ 
 export const metadata: Metadata = {
   title: "Advika Enterprises Cloud Server",
   description: "Advika Cloud provide best cloud server solutions",
-  icons: {
-    icon: [
-      { url: "./favicon.svg", sizes: "16x16", type: "image/svg" },
-      { url: "./favicon.svg", sizes: "32x32", type: "image/svg" },
-      {
-        url: "./favicon.svg",
-        sizes: "192x192",
-        type: "image/png",
-      },
-    ],
-    shortcut: ["./favicon.svg"],
-    apple: [
-      { url: "./favicon.svg" },
-      { url: "./favicon.svg", sizes: "180x180", type: "image/svg" },
-    ],
-    other: [
-      {
-        rel: "apple-touch-icon-precomposed",
-        url: "./favicon.svg",
-      },
-    ],
-  },
 };
+
+
 
 export default function RootLayout({
   children,
